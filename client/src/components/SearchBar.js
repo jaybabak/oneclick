410f063e56c43ff54f937923ad89573dab9fcf76
Dev <jbabak@Samis-MacBook-Pro.local>
2018-03-13 03:29:28 -0400

    Added horseman library and connected front-end to node, made first successful call as crawler bot

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -26,8 +26,6 @@ class SearchBar extends Component {
     this.setState({
       user
     });
-
-    console.log(this.state.user);
   }
 
   handleSubmit(event){
@@ -35,14 +33,32 @@ class SearchBar extends Component {
     event.preventDefault();
     alert('We are currently working on making your search query for ' + this.state.user.keyword + ' come to life! Adding magic now....');
 
+    console.log(this.state.user);
+
+
   }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ user: {response: res.express }}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
 
   render() {
     return (
       <div onSubmit={this.handleSubmit} className="search-bar">
         <form>
           <label>
-            What are you searching for today?
+            What are you searching for today? {this.state.user.response}
           </label>
           <input placeholder="Begin your search..." className="search-textfield" name="keyword" type="text" value={this.state.value} onChange={this.handleChange} />
           <input className="search-submit" type="submit" value="Submit" />
--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
-
+var phantom = require('phantom');
 const app = express();
 const port = process.env.PORT || 5000;
 
+var Horseman = require('node-horseman');
+var horseman = new Horseman();
+
 app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+
+
+  horseman
+    .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
+    .open('http://www.google.com')
+    .count('a')
+    .log() // prints out the number of results
+    .close();
+
+    console.log(horseman);
+
+  res.send({ express: 'Hello From Expressasdsf' });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
