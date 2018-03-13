a845c2bd39d3f903874045636e31c119049cb405
Dev <jbabak@Samis-MacBook-Pro.local>
2018-03-12 22:30:02 -0400

    Added the searchBar component and added some styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SearchBar from './components/SearchBar.js';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,12 +8,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">ONECLICK SEARCH</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section>
+        <SearchBar/>
+        </section>
       </div>
     );
   }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import './SearchBar.css';
+
+class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      user: {
+        keyword: '',
+        location: 'Ottawa, ON',
+      }
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e){
+
+    const field = e.target.name;
+    const user = this.state.user;
+    user[field] = e.target.value;
+
+    this.setState({
+      user
+    });
+
+    console.log(this.state.user);
+
+  }
+
+  handleSubmit(event){
+
+    event.preventDefault();
+    alert('We are currently working on making your search query for ' + this.state.user.keyword + ' come to life! Adding magic now....');
+
+  }
+
+  render() {
+    return (
+      <div onSubmit={this.handleSubmit} className="search-bar">
+        <form>
+          <label>
+            What are you searching for today?
+          </label>
+          <input className="search-textfield" name="keyword" type="text" value={this.state.value} onChange={this.handleChange} />
+          <input className="search-submit" type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
