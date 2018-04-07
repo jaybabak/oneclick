795f49f0265e5662ef9afce31760b602f3ba705b
Dev <jbabak@Samis-MacBook-Pro.local>
2018-04-07 03:42:59 -0400

    Updated and added build /serving html index file

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ const opHelper = new OperationHelper({
     locale: 'CA'
 });
 
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 
 app.get('/api/hello', (req, res) => {
